69d36dd3a9fe51cee9fa1955aa785e54fc342bcb
Mario <lombardomario01@gmail.com>
2022-02-14 16:39:55 +0100

    Routes "Register/Login/Logout" added

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use App\Http\Controllers\VideogameController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 // Auth::routes(); 
 
 Route::get('/', 'VideogameController@home')->name('home');
 
+// Route Register - Login - Logout
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Route api
 Route::get('/api/videogames/list', 'VideogameController@getVideogames')->name('api.videogames.list');
