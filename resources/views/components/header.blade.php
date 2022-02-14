9bca3eac3fa549c0a8ec137dd0e718d0f4a73b2e
Mario <lombardomario01@gmail.com>
2022-02-14 16:12:44 +0100

    Components "Header/Footer" added

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,50 @@
+<header>
+
+    <h1>Videogames</h1>
+
+    @auth
+
+    <div>
+        Benvenuto {{ Auth::user()->name }}
+    </div>
+    
+    <a href="#">LOGOUT</a>
+
+    @else
+
+    <h2>REGISTER</h2>
+    <form action="#" method="POST">
+        
+        @method('POST')
+        @csrf
+
+        <label for="name">Name</label>
+        <input type="text" name="name">
+        <label for="email">Email</label>
+        <input type="email" name="email">
+        <label for="password">Password</label>
+        <input type="password" name="password">
+        <label for="password_confirmation">Confirm Password</label>
+        <input type="password" name="password_confirmation">
+        <input class="btn btn-primary" type="submit" value="REGISTRATI">
+
+    </form>
+
+    <br><br>
+
+    <h2>LOGIN</h2>
+    <form action="#" method="POST">
+
+        @method('POST')
+        @csrf
+
+        <label for="email">Email</label>
+        <input type="email" name="email">
+        <label for="password">Password</label>
+        <input type="password" name="password">
+        <input class="btn btn-primary" type="submit" value="LOGIN">
+
+    </form>
+    @endauth
+
+</header>
