b804b665f0b701799fa05d4fd30b79c3c61dc645
Mario <lombardomario01@gmail.com>
2022-02-14 16:09:34 +0100

    Controller "Videogame" added

---
+++ app/Http/Controllers/VideogameController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Videogame;
+use Illuminate\Http\Request;
+
+class VideogameController extends Controller
+{
+    public function home() {
+
+        return view('pages.home');
+    }
+
+    public function getVideogames() {
+
+        $videogames = Videogame::all();
+
+        dd($videogames);
+
+        return json_encode($videogames);
+    }
+}
